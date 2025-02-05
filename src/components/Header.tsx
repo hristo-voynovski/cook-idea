3afb102ff150ceea4c619b3d88fe41454a759d13
HristoV <hristo.voynovski@gmail.com>
2025-02-05 18:06:05 +0200

    Adjusted header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,20 +8,24 @@ const Header: React.FC = () => {
 
   return (
     <header className="sticky top-0 z-50 bg-white shadow-md">
-      <nav className="container mx-auto px-4 py-3">
-        <div className="flex items-center justify-between">
+      {/* <nav className="container px-4 py-3 w-[100vw]"> */}
+        <div className="grid grid-cols-3 justify-between items-center px-4 py-3">
           <h1
-            className="text-2xl font-bold text-green-600"
+            className="text-2xl font-bold text-green-600 flex grow-2 flex-start cursor-pointer"
             onClick={() => navigate("/")}
           >
             CookIdea
           </h1>
-          <div className="flex-1 max-w-xl ml-4">
+          <div className="flex-grow self-center max-w-3xl">
             <SearchComponent />
           </div>
           {/* Add other navigation items here */}
+          <div className="flex justify-end gap-4 grow-2">
+            <h2 className="text-xl font-bold text-green-500 cursor-pointer"
+            onClick={() => navigate("/")}>Random Recipe</h2>
+          </div>
         </div>
-      </nav>
+      {/* </nav> */}
     </header>
   );
 };
